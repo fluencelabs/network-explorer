e31f0fa5a0781da4d7d2f6bee7396c5fe5773ed4
Akim Mamedov <akim99999999@gmail.com>
2024-03-25 20:32:59 +0700

    Fix import bug

--- src/clients/dealExplorerClient/rpcClient/dealRpcClient.ts
+++ src/clients/dealExplorerClient/rpcClient/dealRpcClient.ts
@@ -1,8 +1,9 @@
-import { Capacity__factory, Deal__factory } from '@fluencelabs/deal-aurora'
 import {
-  type Multicall3ContractCall,
-  Multicall3ContractClientABC,
-} from '@fluencelabs/deal-aurora/dist/utils/rpcClientABC'
+  Capacity__factory,
+  Deal__factory,
+  IMulticall3__factory,
+} from '@fluencelabs/deal-aurora'
+import { type Multicall3ContractCall } from '@fluencelabs/deal-aurora/dist/utils/rpcClientABC'
 import type { ethers } from 'ethers'
 
 import type { CapacityCommitmentStatus } from '../types/schemes.js'
@@ -12,6 +13,92 @@ import {
   serializeTxDealStatus,
   serializeTxToBigInt,
 } from './serializers.js'
+
+// TODO: This class should be imported directly from TS client. To do so, this class should be exported from top level index.js in ts-client.
+// NOTE: The types any there don't matter.
+
+class Multicall3ContractClientABC {
+  _caller
+  _multicall3Contract
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  constructor(caller: any, multicall3ContractAddress: any) {
+    this._caller = caller
+    this._multicall3Contract = IMulticall3__factory.connect(
+      multicall3ContractAddress,
+      this._caller,
+    )
+  }
+  /*
+   * @dev Put arrays of encoded calls and other data to decode and convert responses:
+   * @dev - callResultsInterfaces
+   * @dev - contractMethods
+   * @dev - txResultsConverters
+   * @dev Note, If one of the transaction is reverted it sends null to txResultsConverter.
+   * @dev  Thus, null should be caught appropriate in a supplied txResultsConverter.
+   */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  async _callBatch(
+    callsEncoded: any,
+    callResultsInterfaces: any,
+    contractMethods: any,
+    txResultsConverters: any,
+  ) {
+    if (
+      (callsEncoded.length +
+        callResultsInterfaces.length +
+        contractMethods.length +
+        txResultsConverters.length) %
+        4 !=
+      0
+    ) {
+      throw new Error(
+        'Assertion: callsEncoded, callResultsInterfaces, contractMethods, txResultsConverters should have the same length.',
+      )
+    }
+
+    const multicallContractCallResults =
+      await this._multicall3Contract.aggregate3.staticCall(callsEncoded)
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const decodedResults: any[] = []
+    for (let i = 0; i < multicallContractCallResults.length; i++) {
+      const txResultsConverter = txResultsConverters[i]
+      const contractMethod = contractMethods[i]
+      const callResultsInterface = callResultsInterfaces[i]
+      if (
+        txResultsConverter == undefined ||
+        contractMethod == undefined ||
+        callResultsInterface == undefined
+      ) {
+        throw new Error(
+          'Assertion: txResultsConverter or contractMethod or callResultsInterface is undefined.',
+        )
+      }
+      const rawResult = multicallContractCallResults[i]
+      if (rawResult == undefined || !rawResult.success) {
+        decodedResults.push(txResultsConverter(null))
+        continue
+      }
+      const rawReturnData = rawResult.returnData
+
+      const decoded = callResultsInterface.decodeFunctionResult(
+        contractMethod,
+        rawReturnData,
+      )
+
+      decodedResults.push(txResultsConverter(decoded))
+    }
+    return decodedResults
+  }
+  // Basic implementation of TxResultsConverter.
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  _decodeToString(result: any, defaultNoResult = '') {
+    if (!result) {
+      return defaultNoResult
+    }
+    return result.toString()
+  }
+}
 
 export class DealRpcClient extends Multicall3ContractClientABC {
   constructor(
