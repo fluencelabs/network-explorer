ca31feee50d189e4c3169ef8002a100d37a9bcac
Ivan <39461389+AlcibiadesCleinias@users.noreply.github.com>
2024-03-28 22:17:45 +0100

    feat: ux spam issue and format coins [fixes CHAIN-441, CHAIN-433] (#22)

--- src/pages/offers/Offers.tsx
+++ src/pages/offers/Offers.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from '@emotion/styled'
-import { OffersFilters } from '@fluencelabs/deal-aurora/dist/dealExplorerClient/types/filters'
+import { OffersFilters } from '@fluencelabs/deal-ts-clients/dist/dealExplorerClient/types/filters'
 
 import { Filters } from '../../components/Filters'
 import { PageHeader } from '../../components/PageHeader'
@@ -10,11 +10,18 @@ import { Switch } from '../../components/Switch'
 import { Text } from '../../components/Text'
 import { useFilters } from '../../hooks/useFilters'
 
+import { FILTER_ONLY_APPROVED_PROVIDERS_DEFAULT } from '../../constants/config.ts'
+
 import { OffersFilterModal } from './OffersFilterModal'
 import { OffersTable } from './OffersTable'
 
 export const Offers: React.FC = () => {
   const [filters, setFilter, resetFilters] = useFilters<OffersFilters>()
+
+  // Set default value for onlyApproved filter.
+  if (filters.onlyApproved == undefined) {
+    setFilter('onlyApproved', FILTER_ONLY_APPROVED_PROVIDERS_DEFAULT)
+  }
 
   return (
     <>
@@ -38,10 +45,12 @@ export const Offers: React.FC = () => {
       <Space height="16px" />
       <Switch
         label="Offers from approved providers"
-        value={filters.onlyApproved ?? false}
-        onSwitch={(value) =>
-          setFilter('onlyApproved', !value ? undefined : true)
+        value={
+          filters.onlyApproved === undefined
+            ? FILTER_ONLY_APPROVED_PROVIDERS_DEFAULT
+            : filters.onlyApproved
         }
+        onSwitch={(value) => setFilter('onlyApproved', value)}
       />
       <Space height="64px" />
       <OffersTable filters={filters} />
