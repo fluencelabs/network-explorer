21abe805aa06cb5c7d9a69a9e7d96ec00a6c23d4
Michel Erokhov <mixaileroxov@gmail.com>
2024-09-04 19:28:07 +0200

    refactor(Capacities): remove useStakerClient

--- src/components/ClientProvider.tsx
+++ src/components/ClientProvider.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { createContext, FC, ReactNode } from 'react'
-import { StakerClient } from '@fluencelabs/deal-ts-clients'
+import { DealExplorerClient } from '@fluencelabs/deal-ts-clients'
 
-import { useStakerClient } from '../hooks'
+import { useClient } from '../hooks'
 
-export const ClientContext = createContext<StakerClient | undefined>(undefined)
+export const ClientContext = createContext<DealExplorerClient | undefined>(
+  undefined,
+)
 
 export const ClientProvider: FC<{ children: ReactNode }> = ({ children }) => {
-  const client = useStakerClient()
+  const client = useClient()
 
   return (
     <ClientContext.Provider value={client}>{children}</ClientContext.Provider>
