37f7da47ccb8da6e934dd47c6725aceeb274aa2f
Michel Erokhov <mixaileroxov@gmail.com>
2024-09-16 15:43:18 +0200

    feat(index.html): capitalize first letter

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,34 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import svgr from 'vite-plugin-svgr'
 
+export function capitalizeFirstLetter(word: string) {
+  return word.charAt(0).toUpperCase() + word.slice(1)
+}
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: ['@emotion/babel-plugin'],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [
+      react({
+        babel: {
+          plugins: ['@emotion/babel-plugin'],
+        },
+      }),
+      svgr(),
+      nodePolyfills(),
+      {
+        name: 'html-transform',
+        transformIndexHtml(html) {
+          return html.replace(
+            /<title>(.*?)<\/title>/,
+            `<title>Fluence ${capitalizeFirstLetter(env.VITE_CHAIN)} Explorer </title>`,
+          )
+        },
       },
-    }),
-    svgr(),
-    nodePolyfills(),
-  ],
+    ],
+  }
 })
