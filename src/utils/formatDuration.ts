c7689d07bd7a547769650a60084f2ecd6ca2b538
Michel Erokhov <mixaileroxov@gmail.com>
2024-09-16 09:54:59 +0200

    feat(formatDuration): refactor

--- src/utils/formatDuration.ts
+++ src/utils/formatDuration.ts
@@ -1,7 +1,7 @@
 import { formatDistanceStrict } from 'date-fns'
 
 export function formatDuration(duration: number) {
-  return formatDistanceStrict(Date.now() + duration, Date.now(), {
+  return formatDistanceStrict(duration, 0, {
     unit: duration > 60 * 60 * 24 * 1000 ? 'day' : undefined,
   })
 }
