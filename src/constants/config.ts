0b4459fbd9d3027f6bba88b008f1050703cb2adf
Michel Erokhov <mixaileroxov@gmail.com>
2024-10-15 18:01:21 +0200

    feat(ts-client): update ts-client to 0.22.0

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,20 +1,36 @@
+import {
+  BLOCK_SCOUT_URLS,
+  CHAIN_IDS,
+  ContractsENV,
+  DEPLOYMENTS,
+  RPC_URLS,
+  SUBGRAPH_URLS,
+} from '@fluencelabs/deal-ts-clients'
 import { Chain } from 'viem'
 import { createConfig, createStorage, http } from 'wagmi'
 
 // The network name for fluence contract clients: {kras, dar, stage, local}
-export const FLUENCE_CLIENT_NETWORK =
+export const FLUENCE_CLIENT_NETWORK: ContractsENV =
   import.meta.env.VITE_FLUENCE_CLIENT_NETWORK ?? 'local'
 export const BLOCKSCOUT_URL =
-  import.meta.env.VITE_BLOCKSCOUT_URL ?? 'http://localhost:4000/'
-export const RPC_URL = import.meta.env.VITE_RPC_URL ?? 'http://localhost:8545'
-export const CHAIN_ID = parseInt(import.meta.env.VITE_CHAIN_ID ?? '0x1')
-
+  (import.meta.env.VITE_BLOCKSCOUT_URL ?? FLUENCE_CLIENT_NETWORK === 'local')
+    ? null
+    : BLOCK_SCOUT_URLS[FLUENCE_CLIENT_NETWORK]
+export const RPC_URL =
+  import.meta.env.VITE_RPC_URL ?? RPC_URLS[FLUENCE_CLIENT_NETWORK]
+export const CHAIN_ID = parseInt(
+  import.meta.env.VITE_CHAIN_ID ?? `${CHAIN_IDS[FLUENCE_CLIENT_NETWORK]}`,
+)
 export const FILTER_ONLY_APPROVED_PROVIDERS_DEFAULT =
   import.meta.env.VITE_FILTER_ONLY_APPROVED_PROVIDERS_DEFAULT != undefined
     ? import.meta.env.VITE_FILTER_ONLY_APPROVED_PROVIDERS_DEFAULT == 'true'
     : true
 export const FORMAT_PAYMENT_TOKEN_TO_FIXED_DEFAULT = 3
 export const FORMAT_NATIVE_TOKEN_TO_FIXED_DEFAULT = 6
+
+export const SUBGRAPH_URL =
+  import.meta.env.VITE_SUBGRAPH_URL ?? SUBGRAPH_URLS[FLUENCE_CLIENT_NETWORK]
+export const DEPLOYMENT = DEPLOYMENTS[FLUENCE_CLIENT_NETWORK]
 
 const CHAIN = {
   id: CHAIN_ID,
@@ -23,9 +39,16 @@ const CHAIN = {
   rpcUrls: {
     default: RPC_URL,
   },
-  blockExplorers: {
-    default: BLOCKSCOUT_URL,
-  },
+  ...(BLOCKSCOUT_URL === null
+    ? {}
+    : {
+        blockExplorers: {
+          default: {
+            name: 'BlockScout',
+            url: BLOCKSCOUT_URL,
+          },
+        },
+      }),
 } as const satisfies Chain
 
 const storage = createStorage({
