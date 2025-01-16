f06ec4029b4a961db1b3f51b4f90a0ada195de40
Michel Erokhov <mixaileroxov@gmail.com>
2025-01-16 17:32:03 +0300

    feat(DataCenters): add DataCenterTable (#135)
    * feat(DataCenters): add DataCenterTable
    
    * feat(Menu): add DataCenter into Routing
    
    * feat(DataCentersTable): add datacenters
    
    * fix(DataCenters): fix types
    
    * feat(OfferResourceTable): better price formatting
    
    * feat(resources): add Resources page (#136)
    
    feat(resources): add resources page and update datacenters
    
    * feat(ts-client): update ts-client
    
    * fix(DataCentersTable): fix paggination
    
    * feat(datacenter): show datacenter info
    
    * feat(datacenters): show providers/peers
    
    * fix(DataCenter): remove unfinished Datacenter page
    
    * feat(ts-client): update deal-ts-client
    
    * fix(JsonToYamlView): remove ErrorBoundary

---
+++ src/pages/resources/ResourceTable.tsx
@@ -0,0 +1,88 @@
+import React from 'react'
+
+import {
+  Cell,
+  Row,
+  RowBlock,
+  RowHeader,
+  RowTrigger,
+  ScrollableTable,
+  TableBody,
+  TableColumnTitle,
+  TableHeader,
+} from '../../components/Table'
+import { Text } from '../../components/Text'
+import { JsonToYamlView } from '../../components/YamlView'
+import { useApiQuery } from '../../hooks'
+import { getResourceName } from '../../utils/getResourceName'
+import { formatHexData } from '../../utils/helpers'
+
+const template = [
+  'minmax(10px, 1fr)',
+  'minmax(10px, 1fr)',
+  'minmax(200px, 1fr)',
+]
+
+export const ResourceTable: React.FC = () => {
+  const { data, isLoading } = useApiQuery(
+    (client) => {
+      return client.getResources()
+    },
+    [],
+    {
+      key: `resources`,
+      ttl: 1_000 * 60, // 1 minute
+    },
+  )
+
+  return (
+    <ScrollableTable>
+      <TableHeader template={template}>
+        <TableColumnTitle>Resource ID</TableColumnTitle>
+        <TableColumnTitle>Type</TableColumnTitle>
+        <TableColumnTitle>Metadata</TableColumnTitle>
+      </TableHeader>
+      <TableBody skeletonHeight={48} isLoading={isLoading}>
+        {data?.data?.map((resource) => (
+          <ResourceRow key={resource.id} resource={resource} />
+        ))}
+      </TableBody>
+    </ScrollableTable>
+  )
+}
+
+interface ResourceRowProps {
+  resource: {
+    id: string
+    metadata?: string
+    type: number
+  }
+}
+
+const ResourceRow: React.FC<ResourceRowProps> = ({
+  resource: { id, type, metadata },
+}: ResourceRowProps) => {
+  const resourceName = getResourceName(type)
+
+  return (
+    <RowBlock>
+      <RowHeader>
+        <RowTrigger>
+          <Row template={template}>
+            <Cell>
+              <Text size={12}>{formatHexData(id, 8, 10)}</Text>
+            </Cell>
+            <Cell>
+              <Text size={12}>{resourceName}</Text>
+            </Cell>
+            <Cell>
+              <Text size={12}>
+                <JsonToYamlView data={metadata ?? '{}'} />
+              </Text>
+            </Cell>
+          </Row>
+        </RowTrigger>
+      </RowHeader>
+    </RowBlock>
+  )
+}
